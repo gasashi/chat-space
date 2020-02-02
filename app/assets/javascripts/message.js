c8d4ddd7f576dc2e55ab288a5d9ecb7afc9ca0b3
gasashi <gasashi8264@gmail.com>
2020-02-02 13:52:55 +0900

    automatic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
-$(function(){
-  function buildHTML(message){
-    if ( message.image ) {
+$(function(){ 
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
       var html =
       `<div class="message" data-message-id=${message.id}>
         <div class="messages__info">
@@ -18,8 +18,7 @@ $(function(){
         </div>
         <img src=${message.image} >
       </div>`
-      return html;
-    } else {
+    } else if(message.content) {
       var html =
         `<div class="message" data-message-id=${message.id}>
           <div class="messages__info">
@@ -36,13 +35,27 @@ $(function(){
             </p>
           </div>
         </div>`
-      return html;
-    }
-  }
+    } else if(message.image) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="messages__info">
+            <div class="messages__info__member">
+              ${message.user_name}
+            </div>
+            <div class="messages__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <img src=${message.image} >
+        </div>`
+    };
+    return html;
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+    last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: url,
       type: "POST",
@@ -64,4 +77,32 @@ $(function(){
         $('.submit-btn').prop("disabled", false);
       });
   })
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
